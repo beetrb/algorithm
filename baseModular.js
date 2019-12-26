c5616fb6197a867895833c79de3cfba30cad68c7
beetrb0214 <beetrb0214@gmail.com>
2019-12-26 19:03:58 +0900

    Algorithm

---
+++ baseModular.js
@@ -0,0 +1,44 @@
+/*  x와 y , p 라는 세개의 숫자에 대해  (x^y)%p를 계산하라 (이는 모듈러 제곱거듭이다.)
+    여기서 x는 기저(base) 이고 , y는 지수, p는 모듈러다.
+    모듈러 제곱거듭은 모듈러에 대해 수행하는 종류의 제곱거듭이다. 이는 컴퓨터 과학에서
+    유용하며 공개 키 암호화 알고리즘 분야에서 사용된다.
+*/
+
+function jisu(x,y,p){
+//     let dec=y.toString(2);
+//     let decL=dec.length;
+//     let bin=parseInt(dec);
+//     console.log(bin);
+//     let Arr =new Array();
+    
+//     let doubleMod=(x*x)%p;
+//     let tripleMod=(doubleMod*x)%p;
+//     let result=0;
+//     let mod;
+
+//     //binary
+
+//     for(let i=0;i<y;i++){
+//         if(y>=4){
+//             mod=(doubleMod*doubleMod)%p;
+//         }
+//         if()
+//         else if(y==1){
+//            return result=x%p; 
+//         }
+//     }   
+//     console.log(result);
+ }  let dec= "123";
+
+ /**
+  * @type {number}
+  */
+let g;
+
+function add(a, b) {
+    return a + b;
+}
+
+
+ dec
+  jisu(10,5,3);
